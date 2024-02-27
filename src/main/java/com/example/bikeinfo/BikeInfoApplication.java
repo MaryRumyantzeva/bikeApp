f14c4a16c8af17778e1940e523a46f1fcfddf82b
MaryRumyantzeva <rumyantzevamary@yandex.ru>
2024-02-27 12:53:15 +0300

    Initial commit

---
+++ src/main/java/com/example/bikeinfo/BikeInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bikeinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeInfoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/bikeinfo/domain/Bicycle.java
@@ -0,0 +1,29 @@
+package com.example.bikeinfo.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "bicycle")
+public class Bicycle {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "info")
+    private String info;
+
+    @Column(name = "cost")
+    private int cost;
+}
